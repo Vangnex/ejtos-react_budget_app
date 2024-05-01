f77503c44f1b7c583f06aeeed1dc16c66c643081
Didier C <57138040+Vangnex@users.noreply.github.com>
2024-05-01 17:59:50 +0200

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,44 +1,18 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
-import { AppProvider } from './context/AppContext';
-import Budget from './components/Budget';
-import ExpenseTotal from './components/ExpenseTotal';
-import ExpenseList from './components/ExpenseList';
-import AllocationForm from './components/AllocationForm';
-import Remaining from './components/Remaining';
+const Remaining = () => {
+    const { expenses, budget } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
 
-const App = () => {
     return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <Budget />
-                    </div>
-                    <div className='col-sm'>
-                        <Remaining />
-                    </div>
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Allocation</h3>
-                <div className='row '>
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>
-                </div>
-                <h3 className='mt-3'>Change allocation</h3>
-                <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <AllocationForm/>
-                    </div>
-                </div>
-            </div>
-        </AppProvider>
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
     );
 };
 
-export default App;
+export default Remaining
